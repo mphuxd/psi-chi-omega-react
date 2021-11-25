8fd68c00cf0304897379bce94fdd490112d5f9e4
Matthew <matthewpham2012@gmail.com>
2021-11-25 11:29:45 -0800

    Configured theme
    Added tailwindcss themes and added fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
+
+// postcss.config.js
 module.exports = {
   plugins: {
-    require('postcss-import'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer')
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {}
   },
 }
