0e5c8d6d31c1c5d486426b9710220e97d7b4e46a
Matthew <matthewpham2012@gmail.com>
2022-04-19 07:04:13 -0700

    added config variable

---
+++ backend/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+      jwt: {
+        expiresIn: "7d",
+      },
+    },
+  },
+  // ...
+});
