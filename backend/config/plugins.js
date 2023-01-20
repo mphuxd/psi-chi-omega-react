faddf4ad955d6511aea16b4fc6c77a507dcbe1b3
Matthew <matthewpham2012@gmail.com>
2023-01-19 19:00:20 -0800

    build(strapi): migrate to 4.5.3

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -7,4 +7,43 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  name: "strapi::security",
+  config: {
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "connect-src": ["'self'", "https:"],
+        "img-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "dl.airtable.com",
+          "res.cloudinary.com",
+        ],
+        "media-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "dl.airtable.com",
+          "res.cloudinary.com",
+        ],
+        upgradeInsecureRequests: null,
+      },
+    },
+  },
 });
--- frontend/components/Accordion/index.js
+++ frontend/components/Accordion/index.js
@@ -1,5 +1,5 @@
-import Accordion from "./Accordion.js";
-import AccordionItem from "./AccordionItem.js";
+import Accordion from './Accordion.js';
+import AccordionItem from './AccordionItem.js';
 
 export { Accordion, AccordionItem };
 //export both as named exports
